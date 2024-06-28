2de50c461ff89b059f626760f32320b4a1d643a5
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-28 16:32:33 +0200

    feat: add a v-for to print the message of the active contact

--- js/main.js
+++ js/main.js
@@ -178,28 +178,33 @@ createApp ({
         }
     },
     methods: {
-
+        changeContact(indice){
+            this.activeContact = indice;
+        },
+        whatIDo(){
+            console.log(this.contacts[0].messages);
+        }
     },
     mounted(){
 
-        // esegui per 10 volte l'estrapolazione di una mail casuale
-        for(i = 0; i < 10; i++){
-            // usato axios per prendere il risultato dell'api link
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((risposta) =>{
+        // // esegui per 10 volte l'estrapolazione di una mail casuale
+        // for(i = 0; i < 10; i++){
+        //     // usato axios per prendere il risultato dell'api link
+        //     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((risposta) =>{
 
-                // response è un oggetto quindi dal console.log vedo quale proprietà mi serve per prendere solo le mail
-                const result = risposta.data.response;
+        //         // response è un oggetto quindi dal console.log vedo quale proprietà mi serve per prendere solo le mail
+        //         const result = risposta.data.response;
 
-                // inserisci il risultato nell'array vuoto in Data
-                this.mailList.push(result);
+        //         // inserisci il risultato nell'array vuoto in Data
+        //         this.mailList.push(result);
                 
-                // console.log(risposta);
-                // console.log(result);
-                console.log(this.mailList.length);
-            }).catch(function(error){
-                console.log(error.message);
-            })
-        }
+        //         // console.log(risposta);
+        //         // console.log(result);
+        //         console.log(this.mailList.length);
+        //     }).catch(function(error){
+        //         console.log(error.message);
+        //     })
+        // }
             
         
     }
